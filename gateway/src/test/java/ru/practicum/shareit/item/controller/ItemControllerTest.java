3834a5ef8d9356dd40b980b1e780b368a285bb48
Apti <aptigardanov@ya.ru>
2025-11-11 22:16:32 +0300

    Sprint 16 №2

--- gateway/src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.practicum.shareit.item.client.ItemClient;
+import ru.practicum.shareit.client.ItemClient;
 import ru.practicum.shareit.item.dto.CommentCreateDto;
 import ru.practicum.shareit.item.dto.ItemCreateDto;
 import ru.practicum.shareit.item.dto.ItemUpdateDto;
